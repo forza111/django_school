a0416a2d77d071d46143396ea4c202144477e741
forza111 <nikitos.ionkin@yandex.ru>
2020-08-05 18:01:57 +0300

    from work 05.08.2020

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    '''текст и дата публикации
+    CharField для символьных полей
+    DateTimeField для даты и времени'''
+
+class Choise(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    '''текст выбора и результат голосования'''
