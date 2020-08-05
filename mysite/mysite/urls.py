80ae9bf89b4125848966f0b92b9e923d3866d0b3
forza111 <nikitos.ionkin@yandex.ru>
2020-08-05 09:22:31 +0300

    джанго по туториалу

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
-    path('webexample/', include('webexample')),
 ]
+'''Функция path () передает четыре аргумента, два обязательных: route и view и два
+ необязательных: kwargs и name.
+Функция include () позволяет ссылаться на другие URLconfs. '''
