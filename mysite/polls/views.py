42550e3156acca36de89bdc26c5d5642acfd2575
forza111 <nikitos.ionkin@yandex.ru>
2020-09-01 09:00:54 +0300

    01.09.2020

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render
+from django.http import Http404
 from .models import Question
 
 
@@ -11,12 +12,17 @@ def index(request):
             latest_question_list,
                }
     return render(request, 'polls/index.html', context)
-'''ункция render() принимает объект запроса в качестве первого аргумента, имя шаблона в качестве второго 
+'''Функция render() принимает объект запроса в качестве первого аргумента, имя шаблона в качестве второго 
 аргумента и словарь в качестве необязательного третьего аргумента. Она возвращает объект HttpResponse данного 
 шаблона, отображенный в данном контексте.'''
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s" %question_id )
+    try:
+        question = Question.objects.get(pk = question_id)
+    except Question.DoesNotExist:
+        raise Http404('Question does not exist')
+    return render(request, 'polls/detail.html', {'question' : question})
+'''представление вызывает исключение Http404, если вопрос с запрошенным идентификатором не существует.'''
 
 def results(request, question_id):
     response = "You're looking at the results of question %s"
