d7cc010905f332a42c457d5b0db6fdcd746ed816
forza111 <nikitos.ionkin@yandex.ru>
2020-09-03 16:04:58 +0300

    Написание тестов

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -14,7 +14,8 @@ class Question(models.Model):
         return self.question_text
         '''возвращает текст поля question_text при вызове этого объекта'''
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <=self.pub_date<= now
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
