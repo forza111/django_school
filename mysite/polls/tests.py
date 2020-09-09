b6f68cc1ba3946a237f86c27ca7b65118a777794
forza111 <nikitos.ionkin@yandex.ru>
2020-09-09 08:35:39 +0300

    add test class QuestionDetailViewTests(TestCase)

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -24,6 +24,11 @@ class QuestionIndexViewTests(TestCase):
         '''
         response = self.client.get(reverse('polls:index'))
         self.assertEqual(response.status_code,200)
+        '''
+        assertEqual check first argument "response.status code" 
+        and second argument "200". If the values do not compare equal,
+        the test will fail
+        '''
         self.assertContains(response,'No polls are available')
         '''
         assertContains checks that the server response "response" 
@@ -31,6 +36,10 @@ class QuestionIndexViewTests(TestCase):
         "No polls are available" 
         '''
         self.assertQuerysetEqual(response.context['latest_question_list'], [])
+        '''
+        assertQuerysetEqual asserts that first argument return
+        particular list of values second argument
+        '''
 
     def test_past_question(self):
         """
@@ -82,12 +91,12 @@ class QuestionModelTests(TestCase):
         for questions whose pub_date is in the future'''
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(pub_date = time)
-        self.assertIs(future_question.was_published_recently(), True)
+        self.assertIs(future_question.was_published_recently(), False)
         '''
         assertIs compare
         first argument - "(future_question.was_published_recently())" and 
         second argument - "False"
-        if first argument not= second argument Test FAILED
+        if first argument not= second argument: Test FAILED
         '''
 
 
@@ -107,3 +116,22 @@ class QuestionModelTests(TestCase):
         recent_question = Question(pub_date=time)
         self.assertIs(recent_question.was_published_recently(), True)
 
+class QuestionDetailViewTests(TestCase):
+    def test_future_question(self):
+        '''
+        The detail view of a question with a pub_date in the future
+        :returns a 404 not found
+        '''
+        future_question = create_question(question_text='Future question.', days=5)
+        url = reverse('polls:detail',args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+    def test_past_question(self):
+        '''
+        The detail view of a question with a pub_date in the past
+        displays the question's text
+        '''
+        past_question = create_question(question_text='Past Question', days=-5)
+        url = reverse('polls:detail', args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
