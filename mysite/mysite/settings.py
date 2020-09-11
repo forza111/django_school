72ff721e060be95be875286fbdeeabca9c155389
forza111 <nikitos.ionkin@yandex.ru>
2020-09-11 10:00:07 +0300

    Change admin panel. Add style css for detail.html and results.html

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -19,9 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'zi0ls=#3&170sm867fp^l&l0%h2=ce1e7!9t)0l$^#w$^p^7vm'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
